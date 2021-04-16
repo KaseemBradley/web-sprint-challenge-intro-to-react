c4cc9d041cea5f3b8a28a15b84eb1937c5aceca8
Kaseem Bradley <kaseembradley@Kaseems-Air.fios-router.home>
2021-04-16 13:11:35 -0400

    created Character component

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,17 @@
 // Write your Character component here
+import React from 'react';
+
+const Character = (props) => {
+const {name, height, mass, birthYear} = props;
+
+return (
+    <div>
+        <h3>`Name: ${name}`</h3>
+        <p>`Height: ${height}`</p>
+        <p>`Mass: ${mass}`</p>
+        <p>`Birth Year: ${birthYear}`</p>
+    </div>
+);
+};
+
+export default Character;
