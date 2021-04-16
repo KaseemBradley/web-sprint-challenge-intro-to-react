3ddca47a9f926309e7f672d3236770bdd0f3efd0
Kaseem Bradley <kaseembradley@Kaseems-Air.fios-router.home>
2021-04-16 12:12:54 -0400

    created slice of state for characters and fetched data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
+import { CHAR_URL } from './index';
 
 const App = () => {
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
+ const [charData, setCharData] = useState(null);
+
+ useEffect(() => {
+   axios
+   .get(CHAR_URL)
+   .then(res => {
+     setCharData(res.data);
+   })
+   .catch(err => {
+     return err;
+   });
+
+ }, []);
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
